86e3324204a0e3bc35e162a591abc596e51f7edd
Yurii Senenko <yurkosenenko@gmail.com>
2023-01-28 19:42:44 +0200

    edit connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,11 +3,8 @@ const mongoose = require("mongoose");
 const connectMongo = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const mongoDB = await mongoose.connect(process.env.MONGOOSE_URL);
-    if (mongoDB) {
-      console.log("Database connection successful");
-    }
-    return mongoDB;
+    await mongoose.connect(process.env.MONGOOSE_URL);
+    console.log("Database connection successful");
   } catch (error) {
     console.log(error.message);
     process.exit(1);
